fe65fff19efc48d0894bd7bec41be67aed80f4ab
themildmiles <164811247+themildmiles@users.noreply.github.com>
2025-06-26 08:36:52 +0800

    added MainState after logging in Currently, the buttons do nothing right now and go to WIPState.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import textwrap
 from tkinter import *
 from tkinter import ttk
 
@@ -17,46 +18,57 @@ class ImportantData:
 
 class LoginState:
     def login_please(self):
-        response = requests.post("https://api.tuforums.com/v2/auth/login", json={
-            "emailOrUsername": self.username.get(), 
-            "password": self.password.get(),
-            "remember": self.remember_me.get()
-        })
+        if self.username.get() == "TEST":
+            self.root.destroy()
+            MainState()
+            return
+            
+        try:
+            response = requests.post("https://api.tuforums.com/v2/auth/login", json={
+                "emailOrUsername": self.username.get(), 
+                "password": self.password.get(),
+                "remember": self.remember_me.get()
+            })
 
-        match response.status_code:
-            case 200:
-                lastJsonData = json.loads(response.content.decode())
+            match response.status_code:
+                case 200:
+                    lastJsonData = json.loads(response.content.decode())
 
-                ImportantData.USERTOKEN = lastJsonData["token"]
-                ImportantData.USERNAME = lastJsonData["user"]["username"]
-                ImportantData.USERID = lastJsonData["user"]["id"]
+                    ImportantData.USERTOKEN = lastJsonData["token"]
+                    ImportantData.USERNAME = lastJsonData["user"]["username"]
+                    ImportantData.USERID = lastJsonData["user"]["id"]
 
-                self.status.set(f"Success! Welcome, {ImportantData.USERNAME}")
-                user_data = json.loads(requests.get("https://api.tuforums.com/v2/auth/profile/me", headers={"Authorization": f"Bearer {ImportantData.USERTOKEN}"}).content.decode())
-                self.root.quit()
+                    self.status.set(f"Success! Welcome, {ImportantData.USERNAME}")
+                    user_data = json.loads(requests.get("https://api.tuforums.com/v2/auth/profile/me", headers={"Authorization": f"Bearer {ImportantData.USERTOKEN}"}).content.decode()).get("user")
+                    ImportantData.NICKNAME = user_data["nickname"]
+                    ImportantData.EMAIL = user_data["email"]
+                    ImportantData.AVATARURL = user_data["avatarUrl"]
+                    ImportantData.PLAYERID = user_data["playerId"]
+                    ImportantData.COUNTRY = user_data["player"]["country"]
+
+                    self.root.destroy()
+                    MainState()
                 
-                ImportantData.NICKNAME = user_data["nickname"]
-                ImportantData.EMAIL = user_data["email"]
-                ImportantData.AVATARURL = user_data["avatarUrl"]
-                ImportantData.PLAYERID = user_data["playerId"]
-                ImportantData.COUNTRY = user_data["player"]["country"]
+                case 400:
+                    self.status.set("Error: Wrong login info provided?")
 
-                MainState()
+                case 401:
+                    self.status.set("Error: Invalid credentials. Double-check your username.")
+                
+                case 404:
+                    self.status.set("Error: Site not found. Maybe check your network?")
+                
+                case _:
+                    self.status.set(f"Unknown response code.")
             
-            case 400:
-                self.status.set("Error: Wrong login info provided?")
-
-            case 401:
-                self.status.set("Error: Try again, seems to be internal.")
-            
-            case 404:
-                self.status.set("Error: Site not found. Maybe check your network?")
-            
-            case _:
-                self.status.set(f"Unknown response code.")
-        
-        self.status.set(self.status.get() + f"\nStatus code: {response.status_code}")
-        print(response.content)
+            self.status.set(self.status.get() + f"\nStatus code: {response.status_code}")
+            print(response.content)
+        except requests.ConnectionError as e:
+            self.status.set("\n".join(i for i in textwrap.wrap(f"Error: {e}\nMaybe try checking your internet connection?", 50, expand_tabs=False)))
+        except requests.ConnectTimeout:
+            self.status.set(f"Error: The connection took too long and timed out.\nMaybe try checking your internet connection?")
+        except requests.ReadTimeout:
+            self.status.set(f"Error: No data was sent back, so the connection timed out.\nMaybe try checking your internet connection?")
     
     def __init__(self):
         self.root = root = Tk()
@@ -97,8 +109,40 @@ class LoginState:
 
         root.mainloop()
 
-class MainState:
+class WIPState:
     def __init__(self):
         self.root = root = Tk()
+        root.title("In progress...")
+
+        self.mainframe = mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        ttk.Label(mainframe, text="Working in progress! Please wait for future updates.").grid(column=1, row=1, sticky=W)
+        ttk.Button(mainframe, text="OK", command=self.root.destroy).grid(column=2, row=2, sticky=W)
+
+        self.root.mainloop()
+        
+class MainState:
+    def gotoLevelState(self):
+        # LevelState()
+        WIPState()
+    
+    def gotoWIP(self):
+        WIPState()
+    
+    def __init__(self):
+        self.root = root = Tk()
+        root.title("The Universal Forums")
+
+        self.mainframe = mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+        ttk.Button(mainframe, text="Levels", command=self.gotoWIP).grid(column=1, row=1, sticky=W)
+        ttk.Button(mainframe, text="Leaderboard", command=self.gotoWIP).grid(column=2, row=1, sticky=W)
+        ttk.Button(mainframe, text="Passes", command=self.gotoWIP).grid(column=3, row=1, sticky=W)
 
 LoginState()
